b919ec35866dcac613e9d2ad082432d808dd8693
in uh, lee <boutime2017@gmail.com>
2021-08-02 12:48:47 +0900

    css loader와 style loader의 적용

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,13 @@ const config = {
         use: "babel-loader",
         test: /\.js$/, // 정규표현식 test를 통해 js파일에만 바벨을 적용
       },
+      {
+        // css파일에는 두 loader를 적용하는데
+        // use 배열에 나열된 것 중 오른쪽에 있는 것부터 적용이 된다.
+        // 즉, css-loader의 output이 style-loader의 input으로 들어간다.
+        use: ["style-loader", "css-loader"],
+        test: /\.css$/,
+      },
     ],
   },
 };
