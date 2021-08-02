10e8d3eec9f3275c078e2b6541092460f2a46972
in uh, lee <boutime2017@gmail.com>
2021-08-02 18:38:36 +0900

    css를 bundle.js가 아닌 별도의 css파일로 추출하는 방법

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// webpack5 이전 버전에서는 extract-text-webpack-plugin 사용
 
 const config = {
   entry: "./src/index.js",
@@ -18,14 +20,15 @@ const config = {
         test: /\.js$/, // 정규표현식 test를 통해 js파일에만 바벨을 적용
       },
       {
-        // css파일에는 두 loader를 적용하는데
-        // use 배열에 나열된 것 중 오른쪽에 있는 것부터 적용이 된다.
-        // 즉, css-loader의 output이 style-loader의 input으로 들어간다.
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        // 1. css-loader에 의해 처리된 text를
         test: /\.css$/,
       },
     ],
   },
+  plugins: [new MiniCssExtractPlugin({ filename: "style.css" })],
+  // 2. output 디렉토리에 style.css로 저장한다.
+  // 생성자에 인자를 전달하지 않아도 디폴트 값이 적용된다.
 };
 
 module.exports = config;
