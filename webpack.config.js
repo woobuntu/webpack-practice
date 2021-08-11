58529eb88ff05f927eea23f3bc97064fec078187
in uh, lee <boutime2017@gmail.com>
2021-08-11 17:03:16 +0900

    바벨 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
+var path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/, // 정규표현식 test를 통해 js파일에만 바벨을 적용
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
 };
